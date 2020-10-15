59cfd7fccba2f09816c1bc7be8388dbefc955bc7
QuangChienPTIT <pitpiglet2@gmail.com>
2020-10-15 17:29:42 +0700

    Updates

---
+++ precache-manifest.24f9fafdeea91c415baba79d9c36ebfc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1375da885c7c0a7415a7d9141baf73",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "6fc3a156ceb88f9705e0",
+    "url": "/react-movie/static/css/2.721a0fcb.chunk.css"
+  },
+  {
+    "revision": "3f2bf7f35688ae9a6c63",
+    "url": "/react-movie/static/css/main.4b984ba8.chunk.css"
+  },
+  {
+    "revision": "6fc3a156ceb88f9705e0",
+    "url": "/react-movie/static/js/2.8a4c3f53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-movie/static/js/2.8a4c3f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2bf7f35688ae9a6c63",
+    "url": "/react-movie/static/js/main.b9a643ff.chunk.js"
+  },
+  {
+    "revision": "5fd70710d2feb5432c6e",
+    "url": "/react-movie/static/js/runtime-main.1567f7ed.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-movie/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-movie/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-movie/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-movie/static/media/slick.f97e3bbf.svg"
+  }
+]);
